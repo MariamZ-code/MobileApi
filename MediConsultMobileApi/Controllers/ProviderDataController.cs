5bd986abe2062aefbda624767a09ebc552304576
Developer <mariamzaher420@gmail.com>
2023-12-20 11:49:07 +0200

    .

--- MediConsultMobileApi/Controllers/ProviderDataController.cs
+++ MediConsultMobileApi/Controllers/ProviderDataController.cs
@@ -21,81 +21,152 @@ namespace MediConsultMobileApi.Controllers
 
 
         [HttpGet]
-        //string? providerNameEn , string? providerNameAr,
+        //    public async Task<IActionResult> GetAll(string lang, string? providerName, int startpage = 1, int pageSize = 10)
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            var providers = providerRepo.GetProviders();
+
+        //            var providerEn = new List<ProviderDataEnDTO>();
+        //            var providerAr = new List<ProviderDataArDTO>();
+        //            if (lang == "en")
+        //            {
+        //                if (providerName is not null)
+        //                {
+        //                    providers = providers.Where(p => p.Provider_name_en.Contains(providerName));
+        //                }
+        //                foreach (var provider in providers)
+        //                {
+        //                    ProviderDataEnDTO providerEnDto = new ProviderDataEnDTO
+        //                    {
+
+        //                        Provider_id = provider.Provider_id,
+        //                        Provider_name_en = provider.Provider_name_en,
+
+        //                    };
+
+        //                    providerEn.Add(providerEnDto);
+        //                }
+        //                var totalProvidersEn = providers.Count();
+        //                providers = providers.Skip((startpage - 1) * pageSize).Take(pageSize).OrderByDescending(e => e.Provider_name_en);
+        //                var resultEn = new
+        //                {
+        //                    TotalProviders = totalProvidersEn,
+        //                    PageNum = startpage,
+        //                    PageSize = pageSize,
+
+        //                    Providers = providerEn
+        //                };
+
+        //                return Ok(resultEn);
+
+        //            }
+
+        //            if (providerName is not null)
+        //            {
+        //                providers = providers.Where(p => p.Provider_name_ar.Contains(providerName));
+        //            }
+        //            foreach (var provider in providers)
+        //            {
+        //                ProviderDataArDTO providerArDto = new ProviderDataArDTO
+        //                {
+
+        //                    Provider_id = provider.Provider_id,
+        //                    Provider_name_ar = provider.Provider_name_ar,
+
+        //                };
+
+        //                providerAr.Add(providerArDto);
+        //            }
+
+        //            var totalProvidersAR = providers.Count();
+        //            providers = providers.Skip((startpage - 1) * pageSize).Take(pageSize).OrderByDescending(e => e.Provider_name_ar);
+
+
+        //            var resultAr = new
+        //            {
+        //                TotalProviders = totalProvidersAR,
+        //                PageNum = startpage,
+        //                PageSize = pageSize,
+
+        //                Providers = providerAr
+        //            };
+
+        //            return Ok(resultAr);
+        //        }
+        //        return BadRequest(ModelState);
+        //    }
+        //}
+
+        [HttpGet]
         public async Task<IActionResult> GetAll(string lang, string? providerName, int startpage = 1, int pageSize = 10)
         {
             if (ModelState.IsValid)
             {
                 var providers = providerRepo.GetProviders();
-              
-                var providerEn = new List<ProviderDataEnDTO>();
-                var providerAr = new List<ProviderDataArDTO>();
+
                 if (lang == "en")
                 {
                     if (providerName is not null)
                     {
                         providers = providers.Where(p => p.Provider_name_en.Contains(providerName));
                     }
+
                     var totalProvidersEn = providers.Count();
-                    providers = providers.Skip((startpage - 1) * pageSize).Take(pageSize).OrderByDescending(e => e.Provider_name_en);
-                    foreach (var provider in providers)
+
+                    providers = providers
+                        .OrderByDescending(e => e.Provider_name_en)
+                        .Skip((startpage - 1) * pageSize)
+                        .Take(pageSize);
+
+                    var providerEn = providers.Select(provider => new ProviderDataEnDTO
                     {
-                        ProviderDataEnDTO providerEnDto = new ProviderDataEnDTO
-                        {
+                        Provider_id = provider.Provider_id,
+                        Provider_name_en = provider.Provider_name_en,
+                    }).ToList();
 
-                            Provider_id = provider.Provider_id,
-                            Provider_name_en = provider.Provider_name_en,
-
-                        };
-
-                        providerEn.Add(providerEnDto);
-                    }
                     var resultEn = new
                     {
                         TotalProviders = totalProvidersEn,
                         PageNum = startpage,
                         PageSize = pageSize,
-
                         Providers = providerEn
                     };
 
                     return Ok(resultEn);
-
                 }
 
                 if (providerName is not null)
                 {
                     providers = providers.Where(p => p.Provider_name_ar.Contains(providerName));
                 }
-                foreach (var provider in providers)
+
+                var totalProvidersAr = providers.Count();
+
+                providers = providers
+                    .OrderByDescending(e => e.Provider_name_ar)
+                    .Skip((startpage - 1) * pageSize)
+                    .Take(pageSize);
+
+                var providerAr = providers.Select(provider => new ProviderDataArDTO
                 {
-                    ProviderDataArDTO providerArDto = new ProviderDataArDTO
-                    {
-
-                        Provider_id = provider.Provider_id,
-                        Provider_name_ar = provider.Provider_name_ar,
-
-                    };
-
-                    providerAr.Add(providerArDto);
-                }
-
-                var totalProvidersAR = providers.Count();
-                providers = providers.Skip((startpage - 1) * pageSize).Take(pageSize).OrderByDescending(e => e.Provider_name_en);
-
+                    Provider_id = provider.Provider_id,
+                    Provider_name_ar = provider.Provider_name_ar,
+                }).ToList();
 
                 var resultAr = new
                 {
-                    TotalProviders = totalProvidersAR,
+                    TotalProviders = totalProvidersAr,
                     PageNum = startpage,
                     PageSize = pageSize,
-
                     Providers = providerAr
                 };
 
                 return Ok(resultAr);
             }
+
             return BadRequest(ModelState);
         }
+
     }
 }
